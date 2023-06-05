3e1a6dde0778fd30d8fb2bf404d8a976296288cd
Suneel-B <suneel@technicalhub.io>
2023-06-05 11:13:43 +0530

    #include<stdio.h> int main() {     int num1,num2;     scanf("%d",&num1);     scanf("%d",&num2);     // write your code here //{ Editable Code Starts     printf("%d",num1*num2); //} Editable Code Ends     return 0; }

---
+++ Multiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    // write your code here
+//{ Editable Code Starts
+    printf("%d",num1*num2);
+//} Editable Code Ends
+    return 0;
+}
